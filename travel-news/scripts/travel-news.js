ffec09b2d872692e87ebb0cf6da920761fda4ac0
Aaron Ezra Sterczewski <aaron.sterczewski@gmail.com>
2016-05-04 17:21:22 -0700

    Added styles and animation

--- travel-news/scripts/travel-news.js
+++ travel-news/scripts/travel-news.js
@@ -1,3 +1,18 @@
+var elements;
+
 function init() {
-	console.log("hello world!");
+	elements = document.getElementsByTagName("a");
+	for(var i = 0; i < elements.length; i++) {
+		var element = elements[i];
+		element.addEventListener("mouseover", onElementOver);
+		element.addEventListener("mouseout", onElementOut);
+	}
+}
+
+function onElementOver(e) {
+	TweenMax.to(e.srcElement, 0.5, {color:0xFF235D, x:20, ease:Expo.easeOut});
+}
+
+function onElementOut(e) {
+	TweenMax.to(e.srcElement, 0.5, {color:0x000000, x:0, ease:Elastic.easeOut});
 }
