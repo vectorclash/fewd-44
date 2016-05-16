7a090ce3a32b6cdefb1ecdf7a735930845509edc
Aaron Ezra Sterczewski <aaron.sterczewski@gmail.com>
2016-05-16 14:07:59 -0700

    Added rollovers

--- cookie-recipe-columns/js/main.js
+++ cookie-recipe-columns/js/main.js
@@ -1,6 +1,7 @@
 var infoButton;
 var infoButtonBack;
 var heroImage;
+var tableCells;
 
 function init() {
 	heroImage = document.querySelector(".hero-image");
@@ -9,8 +10,23 @@ function init() {
 	TweenMax.set(infoButtonBack, {transformOrigin:"50% 100%"});
 	onImageOut(null);
 
+	tableCells = document.querySelectorAll("td");
+	for(var i = 0; i < tableCells.length; i++) {
+		var td = tableCells[i];
+		td.addEventListener("mouseover", onTDOver);
+		td.addEventListener("mouseout", onTDOut);
+	}
+
 	heroImage.addEventListener("mouseover", onImageOver);
 	heroImage.addEventListener("mouseout", onImageOut);
+}
+
+function onTDOver(e) {
+	TweenMax.to(e.currentTarget, 1, {borderRadius:10, backgroundColor:0xAED7C1, ease:Back.easeOut});
+}
+
+function onTDOut(e) {
+	TweenMax.to(e.currentTarget, 1, {borderRadius:0, backgroundColor:0xECEFD0, ease:Back.easeOut});
 }
 
 function onImageOver(e) {
