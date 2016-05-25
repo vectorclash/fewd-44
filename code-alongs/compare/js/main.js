e696d2799f03cca9ec68cc9d4ad562dd3cf42542
Aaron Ezra Sterczewski <aaron.sterczewski@gmail.com>
2016-05-24 21:30:46 -0700

    added math functions

--- code-alongs/compare/js/main.js
+++ code-alongs/compare/js/main.js
@@ -9,9 +9,31 @@ function init() {
 	sign = document.querySelector(".sign");
 	compare = document.querySelector(".compare");
 
-	compare.addEventListener("click", onCompareClick);
+	// set event listeners
+	compare.addEventListener('click', onCompareClick);
 }
 
+// Event Handlers
+// ----------------------------------------
+// update the page - change the sign
+
 function onCompareClick() {
-	sign.textContent = "Hello!"
+	compare.removeEventListener("click", onCompareClick);
+
+	var firstValue = parseInt(firstNumber.value);
+	var secondValue = parseInt(secondNumber.value);
+
+	var newSymbol = "null";
+	if(firstValue > secondValue) {
+		newSymbol = ">";
+	} else if(firstValue < secondValue) {
+		newSymbol = "<";
+	} else if(firstValue == secondValue) {
+		newSymbol = "=";
+	}
+
+	sign.textContent = newSymbol;
+	TweenMax.from(firstNumber, 0.5, {transformOrigin:"50% 100%", scaleY:0, ease:Back.easeOut});
+	TweenMax.from(sign, 0.5, {alpha:0, ease:Back.easeOut, delay:0.2});
+	TweenMax.from(secondNumber, 0.5, {transformOrigin:"50% 100%", scaleY:0, ease:Back.easeOut, delay:0.4, onComplete:function(){compare.addEventListener("click", onCompareClick);}});
 }
