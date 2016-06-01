2049953c180609e5c784cbe778587e1a17cace31
Aaron Ezra Sterczewski <aaron.sterczewski@gmail.com>
2016-06-01 10:31:36 -0700

    Finished the color switcher and added it to the index

--- code-alongs/color-switcher/js/color-switcher.js
+++ code-alongs/color-switcher/js/color-switcher.js
@@ -1,4 +1,10 @@
-var colors = [0xBDFF0E, 0xFF2063, 0x84FFF3, 0x5E1A8E, 0xFFCE00, 0x93FF75, 0xFFA7FE];
+var colors = [{color:0xBDFF0E, name:"Lime"},
+			  {color:0xFF2063, name:"Radical Red"},
+			  {color:0x84FFF3, name:"Aquamarine"},
+			  {color:0x5E1A8E, name:"Seance"},
+			  {color:0xFFCE00, name:"Supernova"},
+			  {color:0x93FF75, name:"Mint Green"},
+			  {color:0xFFA7FE, name:"Lavender Rose"}] 
 var colorList;
 var currentColor;
 
@@ -7,7 +13,7 @@ function init() {
 	currentColor = document.querySelector(".current-color");
 	for(var i = 0; i < colors.length; i++) {
 		var newColorLI = document.createElement("li");
-		TweenMax.set(newColorLI, {backgroundColor:colors[i]});
+		TweenMax.set(newColorLI, {backgroundColor:colors[i].color});
 		newColorLI.id = i;
 		colorList.appendChild(newColorLI);
 		newColorLI.addEventListener("click", onColorClick);
@@ -18,6 +24,6 @@ function onColorClick(e) {
 	TweenMax.to(e.currentTarget, 0.5, {css:{border:"20px solid black"}, ease:Quad.easeOut});
 	TweenMax.to(e.currentTarget, 0.5, {css:{border:"5px solid black"}, delay:0.6, ease:Bounce.easeOut});
 
-	TweenMax.to("body", 2, {backgroundColor:colors[e.currentTarget.id], ease:Bounce.easeOut});
-	currentColor.textContent = "Current color theme is: " + colors[e.currentTarget.id];
+	TweenMax.to("body", 2, {backgroundColor:colors[e.currentTarget.id].color, ease:Bounce.easeOut});
+	currentColor.textContent = "Current color theme is: " + colors[e.currentTarget.id].name;
 }
