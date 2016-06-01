1784308bfb5f277125384337f79bab08cc24f7cb
Aaron Ezra Sterczewski <aaron.sterczewski@gmail.com>
2016-05-31 19:33:11 -0700

    Added periln noise

--- homework/temperature-converter/js/temperature-converter.js
+++ homework/temperature-converter/js/temperature-converter.js
@@ -5,6 +5,10 @@ var footer;
 var particleNum = 10;
 var currentParticles = 0;
 var maxParticles = 100;
+var activeParticles;
+
+var time = 0.0;
+var interval = 0.005;
 
 function init() {
 	sign = document.querySelector(".sign");
@@ -19,12 +23,15 @@ function init() {
 	fahrenheitField.addEventListener("focus", onFieldFocus);
 	celsiusField.addEventListener("keyup", onCelsiusUpdate);
 	celsiusField.addEventListener("focus", onFieldFocus);
+
+	TweenMax.ticker.addEventListener("tick", loop);
 }
 
 // particle field effect
 // ---------------------
 
 function initParticles(container) {
+	particleContainer = container;
 	for(var i = 0; i < particleNum; i++) {
 		if(currentParticles <= maxParticles) {
 			var particle = document.createElement("div");
@@ -32,7 +39,9 @@ function initParticles(container) {
 			container.appendChild(particle);
 			var ranSize = Math.random();
 			var ranTime = 20 + Math.random() * 40;
-			TweenMax.set(particle, {scaleX:ranSize, scaleY:ranSize, y:200, x:-200+((Math.random()*window.innerWidth)+200)});
+			var ranX = -200+((Math.random()*window.innerWidth)+200);
+			particle.x = ranX;
+			TweenMax.set(particle, {scaleX:ranSize, scaleY:ranSize, y:200, x:ranX});
 			TweenMax.to(particle, ranTime, {y:-(window.innerHeight/2), ease:Back.easeOut, delay:i*0.5});
 			TweenMax.to(particle, ranTime/2, {backgroundColor:Math.random()*0xFFFFFF, alpha:0, scaleX:ranSize-0.2, scaleY:ranSize-0.2, ease:Back.easeOut, delay:ranTime/3+(i*0.5), onComplete:removeParticle, onCompleteParams:[particle, container]});
 		}
@@ -55,6 +64,18 @@ function colorPulse(field) {
 
 	TweenMax.to(field, 0.2, {backgroundColor:0xFF3E5C});
 	TweenMax.to(field, 0.5, {backgroundColor:0xFFFFFF, delay:0.21, ease:Expo.easeOut});
+}
+
+// Render loop
+
+function loop() {
+
+	for(var i = 0; i < particleContainer.children.length; i++) {
+		var particle = particleContainer.children[i];
+		TweenMax.set(particle, {x:particle.x+noise.perlin2(particle.x, time)*100});
+	}
+
+	time += interval;
 }
 
 // event listeners
